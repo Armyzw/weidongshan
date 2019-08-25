6340910cd917bf4584a15b4f536e63b4fad55eaa
Armyzw <ljhcontrol.yzw@outlook.com>
2019-08-25 12:34:36 +0800

    add code

---
+++ code/003_led_c_008_007/led.c
@@ -0,0 +1,30 @@
+
+int main()
+{
+	unsigned int *pGPFCON = (unsigned int *)0x56000050;
+	unsigned int *pGPFDAT = (unsigned int *)0x56000054;
+
+	/* 配置GPF4为输出引脚 */
+	*pGPFCON = 0x100;
+	
+	/* 设置GPF4输出0 */
+	*pGPFDAT = 0;
+
+	return 0;
+}
+
+
+int main()
+{
+	unsigned int *pGPFCON = (unsigned int *)0x56000050;
+	unsigned int *pGPFDAT = (unsigned int *)0x56000054;
+	/* 配置GPF4为输出引脚 */
+	*pGPFCON = 0x100;
+	/* 配置GPF$输出为低电平 */
+	*pGPFDAT = 0;
+
+	return 0;
+	
+}
+
+
